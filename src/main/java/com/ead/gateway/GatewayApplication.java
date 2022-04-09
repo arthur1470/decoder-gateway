ccdbdf3bd34b1350e227ac5ec68a8b6bdb5006cb
Arthur Marques <arthur.l2@hotmail.com>
2022-04-09 12:16:44 -0300

    Configurações comunicação sincrona gateway

--- src/main/java/com/ead/gateway/GatewayApplication.java
+++ src/main/java/com/ead/gateway/GatewayApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class DecoderGatewayApplication {
+public class GatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DecoderGatewayApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 
 }
